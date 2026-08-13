3f3741e7fe4c98618a779a53d25b9af3fd9a652f
Prateek Yadav <161587108+Prateekyadav1503@users.noreply.github.com>
2026-08-13 22:06:09 +0530

    Time: 1 ms (63.49%), Space: 12.1 MB (12.26%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransom, string magazine) {
+        unordered_map<char,int>need;
+        unordered_map<char,int>have;
+        for(int i=0;i<ransom.size();i++){
+        need[ransom[i]]++;}
+
+         for(int i=0;i<magazine.size();i++){
+        have[magazine[i]]++;}
+
+            for(auto i:need)
+            {char c=i.first;
+            int fneed =i.second;
+            int fhave=have[c];
+
+            if(fhave<fneed){
+            return false;
+            }
+         }
+        return true ;
+    }
+};
