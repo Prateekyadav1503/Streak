efb98a45e1f80f6ae8d68950d349945ba67ced62
Prateek Yadav <161587108+Prateekyadav1503@users.noreply.github.com>
2026-09-03 02:41:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (36.05%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending in 0 (except 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        
+        // Reverse only the second half of the number to avoid integer overflow
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};
