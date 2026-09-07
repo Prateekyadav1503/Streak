6284b5b8fd964f2dc347cf355602ca5be5e2e1f1
Prateek Yadav <161587108+Prateekyadav1503@users.noreply.github.com>
2026-09-08 00:07:55 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int mod = 1e9 + 7;
+        int f[26]{};
+        int ans = 0;
+        
+        for (char& c : s) {
+            int i = c - 'a';
+            int add = (ans + 1 + mod - f[i]) % mod;
+            ans = (ans + add) % mod;
+            f[i] = (f[i] + add) % mod;
+        }
+        
+        return ans;
+    }
+};
